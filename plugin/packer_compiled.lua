5c7699d2af2a6efd963b69c3b217e1d386c50afe
eallyy <eallyy@icloud.com>
2024-04-09 20:48:23 +0300

    color schema added

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -140,6 +140,11 @@ _G.packer_plugins = {
     path = "/Users/eally/.local/share/nvim/site/pack/packer/start/undotree",
     url = "https://github.com/mbbill/undotree"
   },
+  ["vim-be-good"] = {
+    loaded = true,
+    path = "/Users/eally/.local/share/nvim/site/pack/packer/start/vim-be-good",
+    url = "https://github.com/ThePrimeagen/vim-be-good"
+  },
   ["vim-fugitive"] = {
     loaded = true,
     path = "/Users/eally/.local/share/nvim/site/pack/packer/start/vim-fugitive",
